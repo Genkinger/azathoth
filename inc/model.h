30ea5c656e0056ba27543396c4a4e3a221bfc975
Lukas Genkinger <genkinger@protonmail.com>
2018-12-31 19:42:13 +0100

    headers

--- inc/model.h
+++ inc/model.h
@@ -1,3 +1,38 @@
+/**
+ * @file model.h
+ * @author Lukas Genkinger (genkinger@protonmail.com)
+ * @brief model representation and loading facilities
+ * @version 1.0
+ * @date 2018-12-31
+ * 
+ * @copyright Copyright (c) 2018, Lukas Genkinger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND,
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED,
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE,
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR,
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES,
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;,
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND,
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT,
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS,
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those,
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Azathoth project.
+ * 
+ */
 #pragma once
 
 #include "common.h"
@@ -11,9 +46,26 @@ struct model_t
     transform_t transform;
 };
 
-
+/**
+ * @brief loads an aps2 model from disk.
+ * 
+ * @param path 
+ * @return model_t* 
+ */
 model_t *az_model_aps2_load(const char* path);
+
+/**
+ * @brief loads an aps1 model from disk.
+ * 
+ * @param path 
+ * @return model_t* 
+ */
 model_t *az_model_aps1_load(const char* path);
 
+/**
+ * @brief frees a model that was previously loaded by az_model_aps*_load
+ * 
+ * @param model 
+ */
 void    az_model_free(model_t *model);
 
--- inc/texture.h
+++ inc/texture.h
@@ -1,3 +1,38 @@
+/**
+ * @file texture.h
+ * @author Lukas Genkinger (genkinger@protonmail.com)
+ * @brief simple opengl texture wrapper
+ * @version 1.0
+ * @date 2018-12-31
+ * 
+ * @copyright Copyright (c) 2018, Lukas Genkinger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND,
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED,
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE,
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR,
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES,
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;,
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND,
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT,
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS,
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those,
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Azathoth project.
+ * 
+ */
 #pragma once
 #include "farbfeld.h"
 
@@ -8,11 +43,20 @@ struct texture_t
 
     GLenum wrap_s = GL_REPEAT;
     GLenum wrap_t = GL_REPEAT;
-    GLenum min_filter = GL_NEAREST;
-    GLenum mag_filter = GL_NEAREST;
+    GLenum min_filter = GL_LINEAR_MIPMAP_LINEAR;
+    GLenum mag_filter = GL_LINEAR;
     GLuint index = 0;
     GLuint id = 0;
 
 };
 
+/**
+ * @brief Loads a farbfeld image from disk and creates a opengl texture from it. (Note: the v coordinate is flipped)
+ * 
+ * @param path 
+ * @param index opengl texture index
+ * @param wrap_s wrapping mode
+ * @param wrap_t wrapping mode
+ * @return texture_t 
+ */
 texture_t az_texture_farbfeld_load(const char* path, GLuint index = 0, GLuint wrap_s = GL_REPEAT, GLuint wrap_t = GL_REPEAT);
